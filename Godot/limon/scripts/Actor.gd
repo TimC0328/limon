f8a9856aee05e99f9d37879f5770cf9afc3f4888
TimC0328 <TimC0328@gmail.com>
2021-05-19 03:50:11 -0400

    Working on NPCs and Dialog

--- Godot/limon/scripts/Actor.gd
+++ Godot/limon/scripts/Actor.gd
@@ -1,23 +1,22 @@
-extends StaticBody2D
+extends KinematicBody2D
 
+class_name Actor
 
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
-onready var player =  get_node("/root/Main/Player")
+var actorName = "actor";
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+	
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
-			if event.button_index == BUTTON_LEFT and event.pressed:
-				player.canMove = false;
-			else:
-				player.canMove = true;
-				
+		_on_clicked();
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
-
+func _on_clicked():
+	print("Clicked on Actor");
--- Godot/limon/scripts/NPC.gd
+++ Godot/limon/scripts/NPC.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Actor
 
 
 # Declare member variables here. Examples:
@@ -8,16 +8,12 @@ onready var player =  get_node("/root/Main/Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	actorName = "NPC";
 
-func _input_event(_viewport, event, _shape_idx):
-	if event is InputEventMouseButton:
-			if event.button_index == BUTTON_LEFT and event.pressed:
-				player.canMove = false;
-			else:
-				player.canMove = true;
-				
+func _on_clicked():
+	print("Clicked on NPC");
+	player.target = name;
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
-
